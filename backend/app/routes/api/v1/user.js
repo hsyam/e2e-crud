6929ea219b56de11e5511454912c1cb98b1cedc5
Hassan Syam <hassan.syam@robustastudio.com>
2022-07-26 03:37:03 +0200

    fix: fix typo issue

--- backend/app/routes/api/v1/user.js
+++ backend/app/routes/api/v1/user.js
@@ -1,8 +1,7 @@
 import {Router} from 'express'
 import * as UsersController from '../../../controllers/api/v1/usersController' 
-import { usersRequestValidator } from '../../../requests/UsersRequestValidator'
+import { usersRequestValidator } from '../../../requests/usersRequestValidator'
 const UsersV1 = Router()
-const UsersRequestValidator = require('../../../requests/usersRequestValidator')
 UsersV1.get('/', UsersController.fetchUsers)
 UsersV1.get('/:id', UsersController.fetchUser)
 UsersV1.post('/', usersRequestValidator, UsersController.createUser)
